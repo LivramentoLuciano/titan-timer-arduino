ad9a7ab165e5e2754d4b97cfeac9d1694a2aa823
Luciano Livramento (nbk) <livramentoluciano10@gmail.com>
2021-03-12 20:13:51 -0300

    version limpio: elimino programas viejos

--- Versiones anteriores/V1_TrainingTimer/README.h
+++
@@ -1,7 +0,0 @@
-/* V1_TrainingTimer
- * ----------------
- * Recibe la senal de "inicio" de actividad y comienza el ciclo de trabajo/descanso, durante X rounds
- * Muestra el conteo en Display 7 segmentos
- * Alarma sonora al finalizar los intervalos de trabajo/descanso (X segundos antes y al finalizar)
- * Todo por defecto T_Trabajo, T_Descanso, Rounds y operaria en segundos (no minutos, por ahora)
- */
